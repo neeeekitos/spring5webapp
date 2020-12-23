7bc19d2657fc618b39c358475621985152dacc08
Nikita TEREKHOV <nikita.a.terekhov@gmail.com>
2020-12-23 09:52:41 +0100

    a little spring boot demo app

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        // returning this String is going to tell to spring that we want to apply
+        // the view called "books"
+        return "books/list";
+    }
+}
+
